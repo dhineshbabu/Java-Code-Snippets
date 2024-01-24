cc3c48105b31b693f5c5a6bdefd4b370914e6c59
Dhineshbabu Muthuraj <dhineshbabumuthuraj@Dhineshbabus-MacBook-Pro.local>
2024-01-23 18:07:38 -0700

    demo for priority queue

--- CollectionsFramework/src/gfgnotes/priorityqueue/PriorityQueueDemo.java
+++ CollectionsFramework/src/gfgnotes/priorityqueue/PriorityQueueDemo.java
@@ -1,16 +1,16 @@
 package gfgnotes.priorityqueue;
 
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class PriorityQueueDemo {
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         pq.add(10);
         pq.add(20);
         pq.add(15);
         System.out.println(pq.peek());
         System.out.println(pq.poll());
         System.out.println(pq.peek());
-
+        System.out.println(pq.poll());
     }
 }
